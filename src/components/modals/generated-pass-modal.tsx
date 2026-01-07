fe4c00a49e3880fb31d1a7e7ef25909c1851a63f
paco soto <francisotomtz@gmail.com>
2026-01-07 14:07:24 -0600

    feat: Se agrega boton de cierre de modal cuando se crea un pase

--- src/components/modals/generated-pass-modal.tsx
+++ src/components/modals/generated-pass-modal.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon, Copy } from "lucide-react";
+import { CheckCircleIcon, Copy, X } from "lucide-react";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import { toast } from "sonner";
@@ -31,9 +31,16 @@ export const GeneratedPassModal: React.FC<GeneratedPassModalProps> = ({
         onClick={(e) => e.preventDefault()}
       />
       <div
-        className="bg-white rounded-2xl max-w-xl w-full p-6 flex flex-col max-h-[90vh] overflow-y-auto z-10 shadow-2xl"
+        className="relative bg-white rounded-2xl max-w-xl w-full p-6 flex flex-col max-h-[90vh] overflow-y-auto z-10 shadow-2xl"
         onClick={(e) => e.stopPropagation()}
       >
+        <button
+          className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+          onClick={() => setOpenGeneratedPass(false)}
+        >
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </button>
         <div className="text-center flex-shrink-0 my-5">
           <h2 className="text-2xl font-bold inline-block">
             {title}
