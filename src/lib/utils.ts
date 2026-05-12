956bc5ebb8a4e3d392ea2f48c2284b325b2bff48
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2026-05-12 17:31:55 -0600

    Fix: se precargan ubis en pase entrada se corrige fecha desde hasta

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -114,10 +114,7 @@ export const formatDateToString = (date: Date): string => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day} 23:59:59`;
 };
 
 export function capitalizeOnlyFirstLetterDelete_(
